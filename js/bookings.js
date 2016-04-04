b447bc474e154d953c4f495168eccb2897462c81
trickyBytes <richard.tiffin@akoolla.com>
2016-04-04 22:19:28 +0100

    1. Starting to add in rest call for screening information 2. Update total cost of booking based on number of seats chosen and cost of seat types - need todo some validation on data inputted for number of seats..needs to be integer only

---
+++ js/bookings.js
@@ -0,0 +1,17 @@
+function updateScreeningInfo(screening){
+	
+	
+	
+	//update hidden form fields for price of
+		//concessions
+		//wheel chairs
+		//standard
+	
+	//Film name
+	//Sceening date
+	
+	//Seats available
+		//standard
+		//concessions
+		//wheelchairs
+}
