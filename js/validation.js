f6bb1c32bb1293173e29af4513a5e48b03ab96ef
trickyBytes <richard.tiffin@akoolla.com>
2016-04-26 22:13:23 +0100

    A bit more work on validation, now cannot submit booking until fields are all valid.

--- js/validation.js
+++ js/validation.js
@@ -1,10 +1,9 @@
 function validateEmail(email){
 	var emailReg = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
-	var valid = emailReg.test(email);
+	return emailReg.test(email);
+}
 
-	if(!valid) {
-        return false;
-    } else {
-    	return true;
-    }
+function validateAsName(name){
+	var reg = new RegExp(/^([A-Za-z]{2}[ éàëA-Za-z]*)$/);
+	return reg.test(name);
 }
