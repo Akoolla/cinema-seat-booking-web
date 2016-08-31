09fe49ba1ab158e988b14bb1d314848342a57337
trickyBytes <richard.tiffin@akoolla.com>
2016-09-01 00:14:06 +0100

    1. Re-factored into seperate admin and booking sites 2. Added admin section to manage cancellation of bookings 3. Added various bits of nice information 4. A few small tweaks

---
+++ admin/js/validation.js
@@ -0,0 +1,9 @@
+function validateEmail(email){
+	var emailReg = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+	return emailReg.test(email);
+}
+
+function validateAsName(name){
+	var reg = new RegExp(/^([A-Za-z]{2}[ éàëA-Za-z]*)$/);
+	return reg.test(name);
+}
---
+++ bookings/js/restconnection.js
@@ -0,0 +1,2 @@
+var restService = "http://178.62.30.126/cinema-rest/";
+var restService = "http://localhost:8080/";
